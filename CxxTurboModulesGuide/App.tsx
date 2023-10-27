3a0b107b1e40df7af9eb3da25bf1b89da6d87833
Tayfun YUGRUK <tayfunyugruk@gmail.com>
2023-10-26 21:24:57 -0400

    C++ Turbo Native Module changes applied

--- CxxTurboModulesGuide/App.tsx
+++ CxxTurboModulesGuide/App.tsx
@@ -25,6 +25,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import NativeSampleModule from './tm/NativeSampleModule';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -76,6 +78,16 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Section title="Cxx TurboModule">
+            <Text style={styles.highlight}>
+              NativeSampleModule.reverseString of "the quick brown fox jumps
+              over the lazy dog" :
+            </Text>
+            {NativeSampleModule.reverseString(
+              'the quick brown fox jumps over the lazy dog ',
+            )}
+          </Section>
+
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
